1b237c12bfed2079b9256d33cb974a2790e6db3a
Andres Emedan <a.j.emedan@gmail.com>
2023-04-19 22:35:03 +0200

    added myProject page where a user can create their own film projects for other users to submit to.

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,5 +1,5 @@
 const cloudinary = require("../middleware/cloudinary");
-const Post = require("../models/Post");
+const Project = require("../models/Project");
 
 module.exports = {
   getProfile: async (req, res) => { 
@@ -8,40 +8,53 @@ module.exports = {
       //Since we have a session each request (req) contains the logged-in users info: req.user
       //console.log(req.user) to see everything
       //Grabbing just the posts of the logged-in user
-      const posts = await Post.find({ user: req.user.id });
+      const projects = await Project.find({ user: req.user.id });
       //Sending post data from mongodb and user data to ejs template
-      res.render("profile.ejs", { posts: posts, user: req.user });
+      res.render("profile.ejs", { projects: projects, user: req.user });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  getMyProject: async (req, res) => { 
+    console.log(req.user)
+    try {
+      //Since we have a session each request (req) contains the logged-in users info: req.user
+      //console.log(req.user) to see everything
+      //Grabbing just the posts of the logged-in user
+      const projects = await Project.find({ user: req.user.id });
+      //Sending post data from mongodb and user data to ejs template
+      res.render("myProject.ejs", { projects: projects, user: req.user });
     } catch (err) {
       console.log(err);
     }
   },
   getFeed: async (req, res) => {
     try {
-      const posts = await Post.find().sort({ createdAt: "desc" }).lean();
-      res.render("feed.ejs", { posts: posts });
+      const projects = await Project.find().sort({ createdAt: "desc" }).lean();
+      res.render("feed.ejs", { projects: projects });
     } catch (err) {
       console.log(err);
     }
   },
-  getPost: async (req, res) => {
+  getProject: async (req, res) => {
     try {
       //id parameter comes from the post routes
       //router.get("/:id", ensureAuth, postsController.getPost);
       //http://localhost:2121/post/631a7f59a3e56acfc7da286f
       //id === 631a7f59a3e56acfc7da286f
-      const post = await Post.findById(req.params.id);
-      res.render("post.ejs", { post: post, user: req.user});
+      const project = await Project.findById(req.params.id);
+      res.render("project.ejs", { project: project, user: req.user});
     } catch (err) {
       console.log(err);
     }
   },
-  createPost: async (req, res) => {
+  createProject: async (req, res) => {
     try {
       // Upload image to cloudinary
       const result = await cloudinary.uploader.upload(req.file.path);
 
       //media is stored on cloudainary - the above request responds with url to media and the media id that you will need when deleting content 
-      await Post.create({
+      await Project.create({
         title: req.body.title,
         image: result.secure_url,
         cloudinaryId: result.public_id,
@@ -55,29 +68,29 @@ module.exports = {
       console.log(err);
     }
   },
-  likePost: async (req, res) => {
+  likeProject: async (req, res) => {
     try {
-      await Post.findOneAndUpdate(
+      await Project.findOneAndUpdate(
         { _id: req.params.id },
         {
           $inc: { likes: 1 },
         }
       );
       console.log("Likes +1");
-      res.redirect(`/post/${req.params.id}`);
+      res.redirect(`/project/${req.params.id}`);
     } catch (err) {
       console.log(err);
     }
   },
-  deletePost: async (req, res) => {
+  deleteProject: async (req, res) => {
     try {
       // Find post by id
-      let post = await Post.findById({ _id: req.params.id });
+      let project = await Project.findById({ _id: req.params.id });
       // Delete image from cloudinary
-      await cloudinary.uploader.destroy(post.cloudinaryId);
+      await cloudinary.uploader.destroy(project.cloudinaryId);
       // Delete post from db
-      await Post.remove({ _id: req.params.id });
-      console.log("Deleted Post");
+      await Project.remove({ _id: req.params.id });
+      console.log("Deleted Project");
       res.redirect("/profile");
     } catch (err) {
       res.redirect("/profile");
