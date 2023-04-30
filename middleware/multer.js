27a10496c3b23f7c7f8a2ef8b1c7acf4b9c9096c
Andres Emedan <a.j.emedan@gmail.com>
2023-04-30 16:11:29 +0200

    You can now view both the image and file link on the project page.

--- middleware/multer.js
+++ middleware/multer.js
@@ -3,6 +3,11 @@ const path = require("path");
 
 module.exports = multer({
   storage: multer.diskStorage({}),
+
+  // Removed file filter in order to upload both .jpg and .docx files. 
+  // Was not working before, will add file filters later.
+
+  
   // fileFilter: (req, file, cb) => {
   //   let ext = path.extname(file.originalname);
   //   if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png" && ext !== ".doc" && ext !== ".docx") {
