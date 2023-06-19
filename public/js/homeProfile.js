eeb0ed3ec64274d6af9b0cec98b1308491578497
Andres Emedan <a.j.emedan@gmail.com>
2023-06-19 19:17:11 +0200

    homeProfile picture upload icon added. Profile pic file uploads on selection of file instead of button submission

---
+++ public/js/homeProfile.js
@@ -0,0 +1,7 @@
+// Script to have the homeProfile-- profile pic input form, submit once a file is chosen. -- this way we don't have to hit the submit button. 
+
+// Listens for changes in the file input field
+document.getElementById('profilePicUpload').addEventListener('change', function() {
+    // Submit the form when a file is selected
+    document.getElementById('profilePicForm').submit();
+  })
